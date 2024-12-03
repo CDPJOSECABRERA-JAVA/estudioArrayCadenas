b5b7785d57a73247030b06a5f3fc6f2960a79cb7
Cristian22g <criiss22g@gmail.com>
2024-12-03 13:30:17 +0100

    Juego tute acabado

--- src/ejercicios/Tute.java
+++ src/ejercicios/Tute.java
@@ -7,10 +7,10 @@ public class Tute {
 
     public Tute(){
 
-        /* Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("¿Cuántos casos de prueba vas a realizar?");
         int casosPrueba = sc.nextInt();
-        sc = null; */
+        sc = null;
 
         String[] baraja = {
                             "1O","2O","3O","4O","5O","6O","7O", "SO", "CO", "RO",
@@ -23,12 +23,16 @@ public class Tute {
         String[] cartasJugadores = new String[jugadores.length];
         Arrays.fill(cartasJugadores, "");
 
+        for (int i = 0; i < casosPrueba; i++) {
+            repartirCartas(baraja, cartasJugadores);              
 
-        repartirCartas(baraja, cartasJugadores);
+            imprimirCartasJugadores(cartasJugadores, jugadores);
+            
+            juego(cartasJugadores, jugadores);
 
-        imprimirCartasJugadores(cartasJugadores, jugadores);
-        
-        //juego(cartasJugadores, jugadores);
+            Arrays.fill(cartasJugadores, "");
+        }
+
     }
 
     // R E P A R T I R   C A R T A S
@@ -45,28 +49,89 @@ public class Tute {
     }
     // C O M P R O B A R   R E P E T I C I Ó N
     private boolean seRepite(String carta, String[] cartasJugadores){
+
+        String cartaActual = "";
+        int salto = 0;
         for (int i = 0; i < cartasJugadores.length; i++) {
-            for (int j = 0; j < 10; j++) {
-                
+            for (int j = 0; j < cartasJugadores[i].length()/2; j++) {
+                cartaActual = cartasJugadores[i].substring(salto, salto+2);
+                if (carta.equals(cartaActual)) return true;
+                salto+=2;
             }
+            salto=0;
         }
         return false;
     }
     // I M P R I M I R   A R R A Y   Y   J U G A D O R E S
     private void imprimirCartasJugadores(String[] cartasJugadores, String[] jugadores){
+        
+        int salto = 0;
+        String carta;
+
         for (int i = 0; i < cartasJugadores.length; i++) {
-            System.out.println(cartasJugadores[i]);
+            System.out.printf("%2s", jugadores[i]);
+            for (int j = 0; j < 10; j++) {
+                carta = cartasJugadores[i].substring(salto, salto+2);
+                System.out.printf(" %2s ", carta);
+                salto+=2;
+            }
+            salto = 0;
+            System.out.println();
         }
+
     }
 
     // J U E G O
-    private void juego(String[][] cartasJugadores, String[]jugadores){
+    private void juego(String[] cartasJugadores, String[]jugadores){
     
         //elección de palo ganador.
     String[] palos = {"Oro", "Basto", "Copa", "Espada"}; 
     String paloGanador = palos[(int)(Math.random()*((palos.length-1) -0 +1) + 0)];
 
     System.out.println("El palo ganador es: " + paloGanador);
-    paloGanador = paloGanador.substring(0,1);
+    String paloGanadorLetra = paloGanador.substring(0,1);
+
+    String cartaActual, paloCartaActual, numCartaActual;
+    int salto = 0;
+    int copaP = 0, espadaP = 0, bastoP = 0, oroP = 0, paloGanadorP = 0;
+    
+    for (int i = 0; i < palos.length; i++) {       
+        for (int j = 0; j < 10; j++) {
+            cartaActual = cartasJugadores[i].substring(salto, salto+2);
+            paloCartaActual = cartaActual.substring(1, 2);
+            numCartaActual = cartaActual.substring(0, 1);
+            
+            if (numCartaActual.equals("C") || numCartaActual.equals("R")) {
+                
+                if (paloCartaActual.equals(paloGanadorLetra)) paloGanadorP++;
+                else if(paloCartaActual.equals("C")) copaP++;
+                else if(paloCartaActual.equals("E")) espadaP++;
+                else if(paloCartaActual.equals("B")) bastoP++;
+                else if(paloCartaActual.equals("O")) oroP++;
+            }
+            salto+=2;
+        }    
+        if (paloGanadorP == 2){
+
+            System.out.println(jugadores[i] + " puede cantar cuarenta en " + paloGanador);
+        }
+
+        if (copaP == 2){
+            System.out.println(jugadores[i] + " puede cantar veinte en copas");
+        } 
+        if (espadaP == 2){
+            System.out.println(jugadores[i] + " puede cantar veinte en espadas");
+        } 
+        if (bastoP == 2){
+            System.out.println(jugadores[i] + " puede cantar veinte en bastos");
+        } 
+        if (oroP == 2){
+            System.out.println(jugadores[i] + " puede cantar veinte en oro");
+        }
+        salto = 0; copaP = 0; espadaP = 0; bastoP = 0; oroP = 0; paloGanadorP = 0;
     }
+    System.out.println("Fin");
+    System.out.println();
+    }
+
 }
